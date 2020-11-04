db2366e64110e502487f943bcb4b66541e7d19ab
JuttaEm <jutta.ruuskanen@gmail.com>
2020-11-04 10:03:04 +0200

    First commit

---
+++ src/main/java/hh/swd20/AmusementParks/AmusementParksApplication.java
@@ -0,0 +1,13 @@
+package hh.swd20.AmusementParks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmusementParksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmusementParksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hh/swd20/AmusementParks/AmusementParksApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.swd20.AmusementParks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmusementParksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
